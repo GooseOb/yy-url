95361ec61c1737658a2479a955c81ea77a6835bd
GooseOb <gooose.ob@gmail.com>
2025-01-02 01:40:36 +0100

    update input check

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Copy current url
 // @namespace    https://greasyfork.org/en/users/901750-gooseob
-// @version      1.0.2
+// @version      1.0.3
 // @description  Copy current website url by pressing yy
 // @author       GooseOb
 // @license      MIT
@@ -10,10 +10,12 @@
 
 (function () {
   let isPressed = false;
-  const isInput = (element) =>
-    element.tagName === "input" ||
-    element.tagName === "textarea" ||
-    element.isContentEditable;
+  const isInput = (element) => {
+    const tagName = element.tagName.toLowerCase();
+    return (
+      tagName === "input" || tagName === "textarea" || element.isContentEditable
+    );
+  };
 
   document.addEventListener("keyup", (e) => {
     if (e.key === "y" && !isInput(e.target)) {
