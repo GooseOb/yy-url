5b51a6c0c9e93c75f4a1715e026c7c4bcbc03fe7
GooseOb <gooose.ob@gmail.com>
2025-01-02 01:39:09 +0100

    ignore inputs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Copy current url
 // @namespace    https://greasyfork.org/en/users/901750-gooseob
-// @version      1.0.1
+// @version      1.0.2
 // @description  Copy current website url by pressing yy
 // @author       GooseOb
 // @license      MIT
@@ -10,8 +10,13 @@
 
 (function () {
   let isPressed = false;
-  document.addEventListener("keyup", function (e) {
-    if (e.key === "y") {
+  const isInput = (element) =>
+    element.tagName === "input" ||
+    element.tagName === "textarea" ||
+    element.isContentEditable;
+
+  document.addEventListener("keyup", (e) => {
+    if (e.key === "y" && !isInput(e.target)) {
       if (isPressed) {
         navigator.clipboard.writeText(window.location.href);
         isPressed = false;
