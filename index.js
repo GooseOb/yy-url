07688a98e1bc8649fa7344c9f90cd7ef57b1b7d3
GooseOb <gooose.ob@gmail.com>
2025-01-02 01:21:51 +0100

    init

---
+++ index.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Copy current url
+// @namespace    https://greasyfork.org/en/users/901750-gooseob
+// @version      1.0.0
+// @description  Copy current website url by pressing yy
+// @author       GooseOb
+// @license      MIT
+// @match        *
+// ==/UserScript==
+
+(function () {
+  let isPressed = false;
+  document.addEventListener("keyup", function (e) {
+    if (e.key === "y") {
+      if (isPressed) {
+        navigator.clipboard.writeText(window.location.href);
+        isPressed = false;
+      } else {
+        isPressed = true;
+        setTimeout(() => {
+          isPressed = false;
+        }, 1000);
+      }
+    }
+  });
+})();
